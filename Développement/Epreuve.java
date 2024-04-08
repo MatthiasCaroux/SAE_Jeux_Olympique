c7f13875eeac3f0380b010a4161bcffc27d5ab7a
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-04-08 13:46:23 +0200

    mise a jour alexy

--- Développement/Epreuve.java
+++ Développement/Epreuve.java
@@ -1,3 +1,33 @@
-public class Epreuve {
-    
+
+public abstract class Epreuve{
+    private char sexe;
+    private Sport sport;
+
+    public Epreuve(Sport sport, char sexe){
+        this.sport = sport;
+        this.sexe = sexe;
+    }
+
+    public void setSport(){
+        this.sport = sport;
+    }
+
+    public void setSexe(){
+        this.sexe = sexe;
+    }
+
+    public Sport getSport(){
+        return this.sport;
+    }
+
+    public char getSexe(){
+        return this.sexe;
+    }
+
+    public abstract void ajouterParticipant(); 
+
+    public abstract void jouer();
+
 }
+
+
