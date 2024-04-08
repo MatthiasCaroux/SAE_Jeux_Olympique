595ddc8dc8af2e1335e5f0c925fa928a0b220721
Snackin32 <niksan.nagarajah@etu.univ-orleans.fr>
2024-04-08 11:35:21 +0200

    Création classes Athlete et Pays

--- Développement/Athlete.java
+++ Développement/Athlete.java
@@ -1,4 +1,62 @@
 public class Athlete {
 
+    private String nom;
+    private String prenom;
+    private char sexe;
+    private Pays pays;
+    private int force;
+    private int endurance;
+    private int agilite;
+    private int score;
+
+    public Athlete(String nom, String prenom, char sexe, Pays pays, int force, int endurance, int agilite) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.sexe = sexe;
+        this.pays = pays;
+        this.force = force;
+        this.endurance = endurance;
+        this.agilite = agilite;
+        this.score = 0;
+    }
     
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getPreom() {
+        return this.prenom;
+    }
+
+    public int getForce() {
+        return this.force;
+    }
+
+    public int getEndurance() {
+        return this.endurance;
+    }
+
+    public int getAgilite() {
+        return this.agilite;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addScore(int score) {
+        this.score += score;
+    }
 }
--- Développement/Pays.java
+++ Développement/Pays.java
@@ -1,3 +1,18 @@
 public class Pays {
     
+    private String nomPays;
+    private int scoreTotal;
+
+    public Pays(String nomPays) {
+        this.nomPays = nomPays;
+        this.scoreTotal = 0;
+    }
+
+    public String getNomPays() {
+        return this.nomPays;
+    }
+
+    public int getScoreTotal() {
+        return this.scoreTotal;
+    }
 }
