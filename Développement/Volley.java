addeb3a550af23a5f440c4107df4f6acd5c8c7ab
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-05-13 11:08:00 +0200

    Volley

--- Développement/Volley.java
+++ Développement/Volley.java
@@ -2,13 +2,16 @@ import java.util.List;
 
 public class Volley extends Sport{
     
-
     public Volley() {
         super("Volley", true);
     }
 
     public void ajouteAthlete(Athlete athlete, Epreuve epreuve) {
-        
+        if (epreuve instanceof EpreuveCollective && epreuve.getNom().equals("Volley")) {
+            epreuve.add(athlete);
+        } else {
+            System.out.println("Erreur : Cette épreuve n'est pas adaptée au volley.");
+        }
     }
 
 
