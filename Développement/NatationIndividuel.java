5b0a509197ddbfa18c949c29011ba1a0c77ccee1
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-05-14 14:25:24 +0200

    Ajout de la methode jouer()

--- Développement/NatationIndividuel.java
+++ Développement/NatationIndividuel.java
@@ -11,5 +11,13 @@ public class NatationIndividuel extends Natation{
     public void classement(){
         // a modif
     }
-    
+
+    @Override
+    public void jouer(){
+        double score = 0;
+        for (Athlete athlete : this.getAthletes()) {
+            score = athlete.getAgilite() * 0.5 + athlete.getEndurance() * 0.35 + athlete.getForce() * 0.15;
+            athlete.setScore((int) score);
+        }
+    }
 }
