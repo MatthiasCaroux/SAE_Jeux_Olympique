67b9b6176ac16ebff49438e0e15d7c145126892b
caroux <mattcaroux@gmail.com>
2024-05-28 19:46:31 +0200

    suppression des classes inutile apres avoir retravailler le diagramme de classe

--- Développement/Athletisme.java
+++
@@ -1,6 +0,0 @@
-public abstract class Athletisme extends Sport{
-
-    public Athletisme(String nomSport, boolean estCollectif) {
-        super(nomSport, estCollectif);
-    }
-}
--- Développement/Escrime.java
+++
@@ -1,9 +0,0 @@
-public abstract class Escrime extends Sport{
-    public Escrime(String nomSport, boolean estCollectif) {
-        super(nomSport, estCollectif);
-    }
-
-    public int meilleurScore(){
-        return 0;
-    }
-}
