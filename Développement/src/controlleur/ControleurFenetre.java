b5db28b173aa0fa30729b8f7859dfcfd41c4dae9
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-06-17 13:58:13 +0200

    Correction boutons retour

--- Développement/src/controlleur/ControleurFenetre.java
+++ Développement/src/controlleur/ControleurFenetre.java
@@ -16,19 +16,23 @@ public class ControleurFenetre implements EventHandler<ActionEvent>{
 
     public void handle(ActionEvent event) {
         if(event.getSource() == this.applicationJeuxOlympique.getSceneFenetreAccueil().lookup("#seConnecter")){
-            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneConnexion(), "Fenetre de connexion");
+            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneConnexion(), "Fenetre de connexion", "seConnecter");
             System.out.println("Lançons la fenetre de connexion");
         }
         else if (event.getSource() == this.applicationJeuxOlympique.getSceneFenetreAccueil().lookup("#sinscrire")) {
-            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneInscription(), "Fenetre d'inscription");
+            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneInscription(), "Fenetre d'inscription", "sinscrire");
             System.out.println("Lançons la fenetre d'inscription");
         }
         else if (event.getSource() == this.applicationJeuxOlympique.getSceneConnexion().lookup("#entrer")) {
-            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneClassement(), "Fenetre de classement");
+            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneClassement(), "Fenetre de classement", "entrer");
             System.out.println("Lançons la fenetre de classement");
         }
         else if(event.getSource() == this.applicationJeuxOlympique.getSceneInscription().lookup("#boutonRetour")){
-            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneFenetreAccueil(), "Fenetre Accueil");
+            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneFenetreAccueil(), "Fenetre Accueil", "boutonRetour");
+            System.out.println("Retour à la fenetre d'Accueil'");
+        }
+        else if (event.getSource() == this.applicationJeuxOlympique.getSceneConnexion().lookup("#boutonRetourConnexion")) {
+            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneFenetreAccueil(), "Fenetre Accueil", "boutonRetourConnexion");
             System.out.println("Retour à la fenetre d'Accueil'");
         }
     }
