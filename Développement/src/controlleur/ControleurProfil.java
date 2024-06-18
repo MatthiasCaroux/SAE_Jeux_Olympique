3ed57fc8c9f1ca10b2c98115ba4a07fd33d73ac6
alexy17 <alexy.wiciak@etu.univ-orleans.fr>
2024-06-18 17:08:50 +0200

    class

--- Développement/src/controlleur/ControleurProfil.java
+++ Développement/src/controlleur/ControleurProfil.java
@@ -9,7 +9,9 @@ import javafx.scene.control.MenuItem;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;      
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;      
 
 
 public class ControleurProfil implements EventHandler<ActionEvent>{
@@ -19,35 +21,37 @@ public class ControleurProfil implements EventHandler<ActionEvent>{
     private String titre;
 
     @FXML
-    private MenuItem choixMonProfil;
+    private Button choixMonProfil;
 
     @FXML
-    private MenuItem choixDeconnexion;
+    private Button choixDeconnexion;
 
     public ControleurProfil(ApplicationJeuxOlympique applicationJeuxOlympique, String titre){
         System.out.println("ControleurProfil");
         this.applicationJeuxOlympique = applicationJeuxOlympique;
         this.titre = titre;
     }
-    
 
-    private void handleChoixDeconnexion(ActionEvent event) {
-        System.out.println("Déconnexion a été sélectionné !");
-        Alert alert = new Alert(AlertType.INFORMATION, "Etes-vous sûr de vouloir vous déconnecter ?");
-        alert.setTitle("Déconnexion");
-        alert.setHeaderText(null);
-        alert.setContentText("Déconnexion a été sélectionné !");
+    private Alert handleChoixDeconnexion(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Etes-vous sûr de vouloir vous déconnecter ?");
+        alert.setTitle("Partie Perdue");
+        DialogPane dialogPane = alert.getDialogPane();
+        dialogPane.setMinWidth(400);
+        dialogPane.setMinHeight(200);
         alert.showAndWait();
+        return alert;
     }
 
     @Override
     public void handle(ActionEvent arg0) {
         if (arg0.getSource() == this.applicationJeuxOlympique.getSceneAccueilAdmin().lookup("#choixMonProfil")) {
             this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneProfil(), "Profil", "choixMonProfil");
-            System.out.println("Lançons la fenetre de connexion");
+            System.out.println("Votre profil");
         } else if (arg0.getSource() == this.applicationJeuxOlympique.getSceneAccueilAdmin().lookup("#choixDeconnexion")) {
-            handleChoixDeconnexion(arg0);
-            this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneFenetreAccueil(), "Fenetre Accueil", "choixDeconnexion"); //mettre qui si il dit ok
+            Alert alert = handleChoixDeconnexion(arg0);
+            if (alert.getResult() == ButtonType.OK) {
+                this.applicationJeuxOlympique.changerFenetre(this.applicationJeuxOlympique.getSceneFenetreAccueil(), "Fenetre Accueil", "choixDeconnexion"); //mettre qui si il dit ok
+            }
         }
     }
 }
